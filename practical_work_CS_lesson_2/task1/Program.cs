7473f18734489b05f9544d446c49d683b0c64a72
fanur <makhiyanov.ff@gmail.com>
2022-06-10 12:58:17 +0300

    task1

---
+++ practical_work_CS_lesson_2/task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine(array[i]);
+    }
+}
+
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+int numberInt = int.Parse(number);
+
+int[] array = new int[5];
+int index = array.Length - 1;
+
+while (numberInt > 0)
+{
+    array
+        [index] = numberInt % 10;
+        numberInt = numberInt / 10;
+        index--;
+}
+
+    printArray(array);
